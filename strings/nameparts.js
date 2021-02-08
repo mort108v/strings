f63a4b242275e1955e6a57c6d9fb262b744b2325
Morten Six <mort108v@stud.kea.dk>
2021-02-08 13:19:39 +0100

    Strings

---
+++ strings/nameparts.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const fullName = "Morten Bo Sixhoej";
+// fullName.split(" ",1);
+
+const firstSpace = fullName.indexOf(" ");
+const lastSpace = fullName.lastIndexOf(" ");
+
+const firstName = fullName.substring(0, firstSpace);
+const middleName = fullName.substring(firstSpace + 1, lastSpace);
+const lastName = fullName.substring(lastSpace + 1);
+
+
+
+console.log(`First name is ${firstName}, Middle name is ${middleName} and Last name is ${lastName}`);
