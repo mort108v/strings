39471e45e03d68abb0d6213309a91b81450d55c2
Morten Six <mort108v@stud.kea.dk>
2021-02-08 21:50:09 +0100

    Big Fat STring Almost Done . . . (Missing the last one)

---
+++ big_fat_string/bigfatstring.js
@@ -0,0 +1,151 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+const inputField = document.getElementById("anyText");
+const userChoiceValue = document.getElementById("choices");
+const submitTheResult = document.getElementById("submit");
+const outputBox = document.getElementById("output");
+
+let nameInput;
+let convertOption;
+let stringResult;
+
+function init() {
+    console.log("Converter Initialized");
+
+    submitTheResult.addEventListener("click", fetchInput);
+}
+
+function fetchInput() {
+    console.log("fetchInput");
+
+    nameInput = inputField.value;
+
+    if (nameInput) {
+        fetchUserChoice();
+    } else {
+        console.log("Inputfield is empty!")
+    }
+}
+
+function fetchUserChoice() {
+    console.log("fetchUserChoice");
+
+    convertOption = userChoiceValue.value;
+
+    if (convertOption == "0") {
+        firstUppercase();
+    } else if (convertOption == "1") {
+        firstFromFull();
+    } else if (convertOption == "2") {
+        ifFullFindLengthOfFirst();
+    } else if (convertOption == "3") {
+        if3FindMiddle();
+    } else if (convertOption == "4") {
+        ifFileNameCheck();
+    } else if (convertOption == "5") {
+        hidePassword();
+    } else if (convertOption == "6") {
+        makeThirdUpperCase();
+    } else if (convertOption == "7") {
+        mkUpperCaseAfterSpaceAndHyphen();
+    }
+
+    // option.value("0") = firstUppercase();
+    // option.value("1") = firstFromFull();
+    // option.value("2") = ifFullFindLengthOfFirst();
+    // option.value("3") = if3FindMiddle();
+    // option.value("4") = ifFileNameCheck();
+    // option.value("5") = hidePassword();
+    // option.value("6") = makeThirdUpperCase();
+    // option.value("7") = mkUpperCaseAfterSpaceAndHyphen();
+}
+
+function firstUppercase() {
+    console.log("firstUppercase");
+
+    const firstLetter = nameInput[0].toUpperCase();
+    const restOfFirstName = nameInput.substring(1).toLowerCase();
+
+    stringResult = `${firstLetter}${restOfFirstName}`;
+
+    submitResult();
+}
+
+function firstFromFull() {
+    console.log("firstFromFull");
+
+    const firstNamePosition = nameInput.indexOf(" ");
+    stringResult = nameInput.substring(0, firstNamePosition);
+
+    submitResult();
+}
+
+function ifFullFindLengthOfFirst() {
+    console.log("ifFullFindLengthOfFirst");
+
+    const firstNamePosition = nameInput.indexOf(" ");
+    stringResult = nameInput.substring(0, firstNamePosition).length;
+
+    submitResult();
+}
+
+function if3FindMiddle() {
+    console.log("if3FindMiddle");
+
+    const firstPosition = nameInput.indexOf(" ") + 1;
+    const lastPosition = nameInput.lastIndexOf(" ");
+
+    const middleName = nameInput.substring(firstPosition, lastPosition);
+
+    stringResult = `Middle name: ${middleName} at position ${firstPosition} and ${lastPosition}`;
+
+    submitResult();
+}
+
+function ifFileNameCheck() {
+    console.log("ifFileNameCheck");
+
+    const fileType = nameInput.lastIndexOf(".");
+    stringResult = nameInput.substring(fileType);
+
+    submitResult();
+}
+
+function hidePassword() {
+    console.log("hidePassword");
+
+    const asteric = "*";
+    stringResult = asteric.repeat(nameInput.length);
+
+    submitResult();
+}
+
+function makeThirdUpperCase() {
+    console.log("makeThirdUpperCase");
+
+    const beforeUppercase = nameInput.substring(0, 2).toLowerCase();
+    const uppercaseLetter = nameInput[2].toUpperCase();
+    const afterUppercase = nameInput.substring(3).toLowerCase();
+
+    stringResult = `${beforeUppercase}${uppercaseLetter}${afterUppercase}`;
+
+    submitResult();
+}
+
+function mkUpperCaseAfterSpaceAndHyphen() {
+    console.log("mkUpperCaseAfterSpaceAndHyphen");
+
+    // const regexp = 
+    // const letterSwap = nameInput.matcAll(" " + 1, "-" + 1).toUpperCase();
+    // stringResult = letterSwap;
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll
+    submitResult();
+}
+
+function submitResult() {
+    console.log(stringResult);
+
+    outputBox.textContent = stringResult;
+}
