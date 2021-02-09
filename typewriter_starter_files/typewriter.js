39ed8189b019498661d854d77363f05ee726b786
Morten Six <mort108v@stud.kea.dk>
2021-02-09 13:06:43 +0100

    The Typewriter assignment started (init function frinished)

--- typewriter_starter_files/typewriter.js
+++ typewriter_starter_files/typewriter.js
@@ -1,1 +1,34 @@
 "use strict";
+
+let typedText = document.querySelector(".typewritten");
+let typed;
+let somethingHappened;
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    console.log("init");
+
+    // Festch txt from HTML
+    typed = typedText.innerHTML;
+    // Clear fetched text
+    typedText.innerHTML = "";
+    //Start Loop function
+    myLoop(typed);
+}
+
+function myLoop() {
+    console.log(typed);
+
+    // Show the N'th letter
+
+    // - Set textContent to substring of 0 to N
+
+    // Imcrement N (++) 
+
+    // Wait before calling loop again
+
+    if (somethingHappened) {
+        setTimeout(myLoop, 500);
+    }
+}
