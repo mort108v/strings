a83a5208eae8b8b7e57697de292a77eceb5e144b
Morten Six <mort108v@stud.kea.dk>
2021-02-11 23:24:27 +0100

    Barchart divs lavet med random . . . mango animation

--- loops/barchart/barchart.js
+++ loops/barchart/barchart.js
@@ -1,28 +1,44 @@
 "use strict";
 
-const bar = document.getElementsByClassName(".bar");
-let barKeep = document.getElementById("bars").children;
-// let allBars = barKeep.children;
+// const bar = document.getElementsByClassName(".bar");
+const bars = document.getElementById("bars");
 
 addEventListener("load", startBarchart);
 
 function startBarchart() {
     console.log("Barchart startet");
 
-    barKeep[2].style.height = "20px";
 
-    myArrayOfBars();
+    createBarElements();
+    changeHeightOfBars();
 }
 
-function myArrayOfBars() {
+function createBarElements() {
     console.log("My Bar Array");
 
-    let myBarArray = Array.from({ length: 40 }, (_, i) => i + 1)
+    let myBarArray = Array.from({ length: 40 }, (_, i) => i + 1);
+    for (let i = 0; i < myBarArray.length; i++) {
+        // Create boxes
+        const bar = document.createElement("div");
+        const barBox = bars.appendChild(bar);
+        barBox.classList.add("bar");
+
+    }
+    // myBarArray = barKeep;
     console.log(myBarArray);
 }
 
 function changeHeightOfBars() {
     console.log("Changing the height of my bars");
 
+    for (let i = 0; i < bars.children.length; i++) {
+        console.log(i);
+        bars.children[i].style.height = getMyNumberOfCustomers() + "px";
+        // console.log(getMyNumberOfCustomers());
+    }
+}
 
+function getMyNumberOfCustomers() {
+    console.log("Get number of customers")
+    return Math.floor(Math.random() * 120);
 }
